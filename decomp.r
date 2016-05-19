4b536be1832f7ba08dd2011658930942c317a512
Guillaume Huysmans <ghuysmans99@gmail.com>
2016-05-19 23:51:09 +0200

    Made decomp.r more functional

--- decomp.r
+++ decomp.r
@@ -1,35 +1,40 @@
 #!/usr/bin/env Rscript
 library(audio)
 library(fastICA)
-library(signal)
 
 args=commandArgs(trailingOnly=T)
-f1=load.wave(args[1])
-f2=load.wave(args[2])
-if (f1$rate != f2$rate) {
-	f1$rate
-	f2$rate
-	stop("sampling rate mismatch!")
-}
-rate=f1$rate
-if (length(f1) != length(f2)) {
-	paste(length(f1)/rate, "s")
-	paste(length(f2)/rate, "s")
-	stop("input size mismatch!")
-}
+if (length(args) < 4)
+	stop("usage: decomp.r n_sources mix1 mix2 [...] output")
+nsrc=as.numeric(args[1])
+outfile=args[length(args)]
 
-X=matrix(c(f1, f2), ncol=2)
-R=fastICA(X, 2, row.norm=F)
-to_samples=function(v){
-	#center and scale
-	v=v-mean(v)
-	v=v/max(abs(max(v)), abs(min(v)))
-}
-s1=to_samples(R$S[,1])
-s2=to_samples(R$S[,2])
+mixes=lapply(args[2:(length(args)-1)], function(x) {
+	cat("Loading", x, "\n")
+	w=load.wave(x)
+	cat("OK: rate", w$rate, "duration", length(w)/w$rate, "s\n")
+	return(w)
+})
 
-m=matrix(c(s1, s2), nrow=2, byrow=T)
-w=as.audioSample(m, rate, min(f1$bits, f2$bits))
-save.wave(w, args[3])
+#validate input and assign "first"
+invisible(sapply(mixes, function(x){
+	if (!exists("first"))
+		first <<- x
+	if (first$rate != x$rate)
+		stop("sampling rate mismatch!")
+	if (length(first) != length(x))
+		stop("input size mismatch!")
+}))
+
+cat("Running fastICA...\n")
+mixes=matrix(sapply(mixes, identity), ncol=length(mixes))
+R=fastICA(mixes, nsrc, row.norm=F)
+out=apply(R$S, 2, function(v){
+	v=v-mean(v) #center
+	v/max(abs(max(v)), abs(min(v))) #scale
+})
+
+cat("Writing", outfile, "\n")
+w=as.audioSample(t(out), first$rate, first$bits)
+save.wave(w, outfile)
 
 R$A
