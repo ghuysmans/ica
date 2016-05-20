f6bb9d39088977c59a7439b79f594c3288762cb4
Guillaume Huysmans <ghuysmans99@gmail.com>
2016-05-20 17:47:24 +0200

    Used tuneR to analyze WAV files
    The audio package seems unable to read its own output...

--- spectrogram.r
+++ spectrogram.r
@@ -1,10 +1,15 @@
 #!/usr/bin/env Rscript
-library(audio)
+suppressMessages(library(tuneR))
 suppressMessages(library(seewave))
 
 args=commandArgs(trailingOnly=T)
 for (x in args) {
-	v=load.wave(x)
+	v=readWave(x)
+	rate=readWave(x, header=T)$sample.rate
 	n=min(1024, length(v))
-	spectro(v, f=v$rate, osc=T, main=x) #, flim=c(0,5))
+	cat(x)
+	print(v)
+	spectro(v@left, f=rate, osc=T, main=paste(x, "(left)"))
+	if (v@stereo)
+		spectro(v@right, f=rate, osc=T, main=paste(x, "(right)"))
 }
